b095cab2296a14a5dd903333e2972ff19bce11d4
Aziza Hurt <azizamhurt@gmail.com>
2019-04-20 19:36:37 -0500

    finshed what i understand

--- infoUser/Program.cs
+++ infoUser/Program.cs
@@ -6,11 +6,7 @@ namespace infoUser
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.Write("get out");
-            Console.ReadLine();
+            User user = new User();              user.FirstName = user.AnswerQuestion("What's your first name?");              user.LastName = user.AnswerQuestion("What's your last name?");              user.FavAnimal = user.AnswerQuestion("What's your favorite animal?");              user.FavColor = user.AnswerQuestion("What's your favorite color?");              user.FavNumber = user.AnswerQuestionWithInteger("What's your favorite number?");              Console.WriteLine($"So you are { user.FirstName} { user.LastName} ");              Console.WriteLine($"Your spirit animal is a { user.FavAnimal}! ");              Console.WriteLine($"Your favorite color is { user.FavColor}");              Console.WriteLine($"And your lucky number is { user.FavNumber}");
         }
-
-        //comment 
-    }
+    }   
 }
--- infoUser/user.cs
+++ infoUser/user.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 namespace infoUser
 {
-    public class user
+    class User
     {
-        public user()
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FavAnimal { get; set; }
+        public string FavColor { get; set; }
+        public int FavNumber { get; set; }
+
+
+        public string AnswerQuestion(string question)
         {
+
+            Console.WriteLine(question);
+            return Console.ReadLine();
+        }
+
+        public int AnswerQuestionWithInteger(string question)
+        {
+            string stringAnswer = AnswerQuestion(question);
+            return int.Parse(stringAnswer);
         }
     }
 }
