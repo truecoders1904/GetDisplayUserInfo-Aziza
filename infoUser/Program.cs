e11c822e380e7368770b92e03221043d44ffded4
Aziza Hurt <azizamhurt@gmail.com>
2019-04-21 00:28:32 -0500

    trying and failing at colors

--- infoUser/Program.cs
+++ infoUser/Program.cs
@@ -7,6 +7,20 @@ namespace infoUser
         static void Main(string[] args)
         {
             User user = new User();              user.FirstName = user.AnswerQuestion("What's your first name?");              user.LastName = user.AnswerQuestion("What's your last name?");              user.FavAnimal = user.AnswerQuestion("What's your favorite animal?");              user.FavColor = user.AnswerQuestion("What's your favorite color?");              user.FavNumber = user.AnswerQuestionWithInteger("What's your favorite number?");              Console.WriteLine($"So you are { user.FirstName} { user.LastName} ");              Console.WriteLine($"Your spirit animal is a { user.FavAnimal}! ");              Console.WriteLine($"Your favorite color is { user.FavColor}");              Console.WriteLine($"And your lucky number is { user.FavNumber}");
+
+
+            if (user.FavNumber > 10 )
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(user.FavNumber);
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(user.FavNumber);
+            }
         }
     }   
 }
