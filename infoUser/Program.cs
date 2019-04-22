8839d97737ddbbc536004be8c0198578057a8b14
Aziza Hurt <azizamhurt@gmail.com>
2019-04-22 12:26:08 -0500

    Fixed color issue

--- infoUser/Program.cs
+++ infoUser/Program.cs
@@ -6,20 +6,18 @@ namespace infoUser
     {
         static void Main(string[] args)
         {
-            User user = new User();              user.FirstName = user.AnswerQuestion("What's your first name?");              user.LastName = user.AnswerQuestion("What's your last name?");              user.FavAnimal = user.AnswerQuestion("What's your favorite animal?");              user.FavColor = user.AnswerQuestion("What's your favorite color?");              user.FavNumber = user.AnswerQuestionWithInteger("What's your favorite number?");              Console.WriteLine($"So you are { user.FirstName} { user.LastName} ");              Console.WriteLine($"Your spirit animal is a { user.FavAnimal}! ");              Console.WriteLine($"Your favorite color is { user.FavColor}");              Console.WriteLine($"And your lucky number is { user.FavNumber}");
-
-
+            User user = new User();              user.FirstName = user.AnswerQuestion("What's your first name?");              user.LastName = user.AnswerQuestion("What's your last name?");              user.FavAnimal = user.AnswerQuestion("What's your favorite animal?");              user.FavColor = user.AnswerQuestion("What's your favorite color?");              user.FavNumber = user.AnswerQuestionWithInteger("What's your favorite number?");              Console.WriteLine($"So you are { user.FirstName} { user.LastName} ");              Console.WriteLine($"Your spirit animal is a { user.FavAnimal}! ");         
             if (user.FavNumber > 10 )
             {
                 Console.BackgroundColor = ConsoleColor.White;
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine(user.FavNumber);
+                Console.WriteLine($"And your lucky number is { user.FavNumber}");
             }
             else
             {
                 Console.BackgroundColor = ConsoleColor.White;
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(user.FavNumber);
+                Console.WriteLine($"And your lucky number is { user.FavNumber}");
             }
         }
     }   
